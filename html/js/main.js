9a4eff7b6a990b607af9abeca357502954b30759
Nicholas Eden <Nicholas.Eden@gmail.com>
2012-11-10 14:48:23 -0500

    Started on Map to View Report section

--- html/js/main.js
+++ html/js/main.js
@@ -7,8 +7,8 @@ function mainCtrl ($scope, $element, $rootScope) {
 	var $frontpage = jqElement.find('#frontpage');
 	var $makeReport = jqElement.find('#make-report');
 	var $viewReports = jqElement.find('#view-reports');
-	$makeReport.hide();
-	$viewReports.hide();
+	//$makeReport.hide();
+	//$viewReports.hide();
 
 	
 	
@@ -31,12 +31,10 @@ function mainCtrl ($scope, $element, $rootScope) {
 function frontpageCtrl ($scope, $element, $rootScope) {
 	
 	$scope.make = function make () {
-		console.log('make');
 		$rootScope.$broadcast('make', 'Hello World');
 	}
 	
 	$scope.view = function view () {
-		console.log('view');
 		$rootScope.$broadcast('view', 'Hello World');
 	}
 	
@@ -48,5 +46,79 @@ function makeReportCtrl ($scope, $element, $rootScope) {
 }
 
 function viewReportsCtrl ($scope, $element, $rootScope) {
+	var map;
+	var geocoder;
+	
+	var lowZoomData = [
+	
+	];
+	
+	var highZoomData = [
+		{location: new google.maps.LatLng(40.7143528, -74.0059731), weight: 1},
+		{location: new google.maps.LatLng(40.5143528, -73.0059731), weight: .2},
+		{location: new google.maps.LatLng(41.7143528, -75.0059731), weight: 1},
+		{location: new google.maps.LatLng(39.7143528, -74.1059731), weight: .5},
+		{location: new google.maps.LatLng(40.7143528, -74.1059731), weight: .4}
+	];
+	
+	var lowZoomData = [
+		
+	
+	];
+	
+	$rootScope.$on('view', function (event, data) {
+		geocoder = new google.maps.Geocoder();
+		//geocoder.geocode
+		var mapOptions = {
+		  center: new google.maps.LatLng(40.7143528, -74.0059731),
+		  zoom: 8,
+		  mapTypeId: google.maps.MapTypeId.ROADMAP
+		};
+		var $mapElement = $($element).find('#map-display');
+		console.log($mapElement);
+		map = new google.maps.Map(document.getElementById("map-display"), mapOptions);
+		
+		
+		var heatmap = new google.maps.visualization.HeatmapLayer({
+		  data: highZoomData,
+		  dissipating: true
+		});
+		
+		heatmap.setMap(map);
+		
+	});
+	
+	if (navigator.geolocation) {
+		console.log('getting location');
+		navigator.geolocation.getCurrentPosition(setGeoLocation, showError);
+    }
 
+	
+	function setGeoLocation(position) {
+		console.log('location found');
+		console.log(position);
+	}
+	
+	function showError(error) {
+		console.log('an error occured');
+		console.log(error);
+	}
+	
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
