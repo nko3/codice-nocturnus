6f19645301d0158e7c84416c58ce8119371d4abd
Nicholas Eden <Nicholas.Eden@gmail.com>
2012-11-10 02:00:22 -0500

    Started on Angular controllers

--- html/js/main.js
+++ html/js/main.js
@@ -1,2 +1,52 @@
 var myApp = angular.module('myApp',[]);
 
+
+
+function mainCtrl ($scope, $element, $rootScope) {
+	var jqElement = $($element);
+	var $frontpage = jqElement.find('#frontpage');
+	var $makeReport = jqElement.find('#make-report');
+	var $viewReports = jqElement.find('#view-reports');
+	$makeReport.hide();
+	$viewReports.hide();
+
+	
+	
+	$rootScope.$on('make', function (event, data) {
+		$frontpage.hide();
+		$makeReport.show();
+	});
+	
+	$rootScope.$on('view', function (event, data) {
+		$frontpage.hide();
+		$viewReports.show();
+	});
+	
+
+}
+
+
+
+
+function frontpageCtrl ($scope, $element, $rootScope) {
+	
+	$scope.make = function make () {
+		console.log('make');
+		$rootScope.$broadcast('make', 'Hello World');
+	}
+	
+	$scope.view = function view () {
+		console.log('view');
+		$rootScope.$broadcast('view', 'Hello World');
+	}
+	
+}
+
+
+function makeReportCtrl ($scope, $element, $rootScope) {
+
+}
+
+function viewReportsCtrl ($scope, $element, $rootScope) {
+
+}
