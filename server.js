b767aa96fe88f89a4fa937d26f157edd2fe7659d
Nicholas Eden <Nicholas.Eden@gmail.com>
2012-11-11 16:41:04 -0500

    Started on file upload

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ Lactate Setup
 
 var lactate = require('lactate');
 var server = lactate.createServer({root:'html'});
-
+var formidable = require('formidable');
 
 server.listen(80, function() {
   console.log('Lactate listening on port 80');
@@ -16,5 +16,14 @@ server.addListener('request', function(req, res) {
 	files.serve('index.html', req, res);
   } else if (req.url.indexOf('/socket.io') === 0) {
 	files.toMiddleware();
+  } else if (req.url == '/upload' && req.method.toLowerCase() == 'post') {
+		// parse a file upload
+		var form = new formidable.IncomingForm();
+		form.parse(req, function(err, fields, files) {
+		  res.writeHead(200, {'content-type': 'text/plain'});
+		  res.write('received upload:\n\n');
+		  res.end(sys.inspect({fields: fields, files: files}));
+		});
+		return;
   }
 });
